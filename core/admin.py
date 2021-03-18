9f0f43a57eef8cfb23086df1294bde1262096b91
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-03-17 21:35:08 -0300

    test product registration

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import produto
+
+@admin.register(produto)
+class produtoAdm(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque', 'slug', 'criado', 'modificado', 'ativo')
