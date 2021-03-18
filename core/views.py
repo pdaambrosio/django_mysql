010e3975a833fd9cffd2d85262b9677c702fd5c9
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-03-17 23:34:41 -0300

    update settings, urls and views

--- core/views.py
+++ core/views.py
@@ -15,13 +15,11 @@ def contato(request):
             # email = form.cleaned_data['email']
             # assunto = form.cleaned_data['assunto']
             # mensagem = form.cleaned_data['mensagem']
-
             # print('Mensagem enviada')
             # print(f'Nome: {nome}')
             # print(f'E-mail: {email}')
             # print(f'Assunto: {assunto}')
             # print(f'Mensagem: {mensagem}')
-
             messages.success(request, 'Formulario enviado com sucesso!')
             form = ContatoForm()
         else:
@@ -36,11 +34,12 @@ def produto(request):
     if str(request.method) == 'POST':
         form = produtoModelForm(request.POST, request.FILES)
         if form.is_valid():
-            prod = form.save(commit=False)
-            print(f'Nome: {prod.nome}')
-            print(f'Preco: {prod.preco}')
-            print(f'Estoque: {prod.estoque}')
-            print(f'Imagem: {prod.imagem}')
+            # prod = form.save(commit=False)
+            # print(f'Nome: {prod.nome}')
+            # print(f'Preco: {prod.preco}')
+            # print(f'Estoque: {prod.estoque}')
+            # print(f'Imagem: {prod.imagem}')
+            form.save()
             messages.success(request, 'Cadastro realizado com sucesso.')
             form = produtoModelForm()
         else:
