e5af1c71f7418915bee81196624c302b6d7268df
Paulo Daniel Ambrosio dos Santos <pda.ambrosio@gmail.com>
2021-03-22 22:00:42 -0300

    update application

--- django_mysql/wsgi.py
+++ django_mysql/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_mysql.settings')
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
